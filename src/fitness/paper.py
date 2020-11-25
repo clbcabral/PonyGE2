8d62cf1373417dbb9fa1c000f5b9964fc2f2112d
Cleber <alberto.cleber@gmail.com>
2020-11-25 08:39:11 -0300

    Otimizando uso da memória.

--- src/fitness/paper.py
+++ src/fitness/paper.py
@@ -1,5 +1,6 @@
 from fitness.base_ff_classes.base_ff import base_ff
-from tensorflow.keras import datasets, layers, models, callbacks, optimizers
+from keras import datasets, layers, models, callbacks, optimizers
+from keras import backend as K 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 import re, csv, os
@@ -17,81 +18,6 @@ class paper(base_ff):
         
         print('FENOTIPO: %s' % ind.phenotype)
 
-        nconv, npool, nfc, nfcneuron = [int(i) for i in re.findall('\d+', ind.phenotype.split('lr-')[0])]
-        has_dropout = 'dropout' in ind.phenotype
-        has_batch_normalization = 'bnorm' in ind.phenotype
-        has_pool = 'pool' in ind.phenotype
-        learning_rate = float(ind.phenotype.split('lr-')[1])
-
-        # Carregando dataset
-        (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
-        validation_images, test_images, validation_labels, test_labels = train_test_split(test_images, test_labels, test_size=0.33, random_state=42)
-
-        # Normalizando
-        train_images = train_images / 255.0
-        test_images = test_images / 255.0
-        validation_images = validation_images / 255.0
-
-        lb = LabelBinarizer()
-        train_labels = lb.fit_transform(train_labels)
-        validation_labels = lb.transform(validation_labels)
-        test_labels = lb.transform(test_labels)
-
-        # num de filtros
-        filter_size = 32
-        nfilter = 0
-
-        # Iniciando o modelo da RN
-        model = models.Sequential()
-
-        try:
-        
-            # Pooling
-            for i in range(npool):
-        
-                # Convolucoes
-                for j in range(nconv):
-        
-                    model.add(layers.Conv2D(filter_size, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
-
-                    nfilter += 1
-
-                    # Numero de filtros duplica a cada duas Convolucoes
-                    if nfilter == 2:
-                        filter_size *= 2
-                        nfilter = 0
-
-                    if has_batch_normalization:
-                        model.add(layers.BatchNormalization())
-
-                # Adiciona o pooling somente se estiver no fenotipo.
-                if has_pool:
-                    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-                    if has_dropout:
-                        model.add(layers.Dropout(0.25))
-
-            model.add(layers.Flatten())
-
-            # fully connected
-            for i in range(nfc):
-                model.add(layers.Dense(nfcneuron))
-                model.add(layers.Activation('relu'))
-
-            if has_dropout:
-                model.add(layers.Dropout(0.5))
-
-            model.add(layers.Dense(10, activation='softmax'))
-            model.summary()
-
-        except Exception as ex:
-            print(ex)
-            return 0
-
-        opt = optimizers.Adam(lr=learning_rate)
-        
-        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-
         acuracia = 0.0
 
         with open(self.arquivo, mode='r') as file:
@@ -104,13 +30,91 @@ class paper(base_ff):
         if not acuracia:
 
             print('Model ainda não foi treinado. Treinando...')
+
+            if K.backend() == 'tensorflow':
+                K.clear_session()
+
+            nconv, npool, nfc, nfcneuron = [int(i) for i in re.findall('\d+', ind.phenotype.split('lr-')[0])]
+            has_dropout = 'dropout' in ind.phenotype
+            has_batch_normalization = 'bnorm' in ind.phenotype
+            has_pool = 'pool' in ind.phenotype
+            learning_rate = float(ind.phenotype.split('lr-')[1])
+
+            # Carregando dataset
+            (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+            validation_images, test_images, validation_labels, test_labels = train_test_split(test_images, test_labels, test_size=0.33, random_state=42)
+
+            # Normalizando
+            train_images = train_images / 255.0
+            test_images = test_images / 255.0
+            validation_images = validation_images / 255.0
+
+            lb = LabelBinarizer()
+            train_labels = lb.fit_transform(train_labels)
+            validation_labels = lb.transform(validation_labels)
+            test_labels = lb.transform(test_labels)
+
+            # num de filtros
+            filter_size = 32
+            nfilter = 0
+
+            # Iniciando o modelo da RN
+            model = models.Sequential()
+
+            try:
+            
+                # Pooling
+                for i in range(npool):
+            
+                    # Convolucoes
+                    for j in range(nconv):
+            
+                        model.add(layers.Conv2D(filter_size, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+
+                        nfilter += 1
+
+                        # Numero de filtros duplica a cada duas Convolucoes
+                        if nfilter == 2:
+                            filter_size *= 2
+                            nfilter = 0
+
+                        if has_batch_normalization:
+                            model.add(layers.BatchNormalization())
+
+                    # Adiciona o pooling somente se estiver no fenotipo.
+                    if has_pool:
+                        model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+                        if has_dropout:
+                            model.add(layers.Dropout(0.25))
+
+                model.add(layers.Flatten())
+
+                # fully connected
+                for i in range(nfc):
+                    model.add(layers.Dense(nfcneuron))
+                    model.add(layers.Activation('relu'))
+
+                if has_dropout:
+                    model.add(layers.Dropout(0.5))
+
+                model.add(layers.Dense(10, activation='softmax'))
+                #model.summary()
+
+            except Exception as ex:
+                print(ex)
+                return 0
+
+            opt = optimizers.Adam(lr=learning_rate)
+            
+            model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
             
             es = callbacks.EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=10, baseline=0.5)
 
             model.fit(train_images, train_labels, epochs=70, batch_size=128, 
                 validation_data=(validation_images, validation_labels), callbacks=[es])
                 
-            _, acuracia = model.evaluate(test_images, test_labels, verbose=2)
+            _, acuracia = model.evaluate(test_images, test_labels, verbose=0)
 
             with open(self.arquivo, mode='a') as file:
                 writer = csv.writer(file)
