3791400dc09449f57d9e22ef8e9b35940f7aa964
Cleber <alberto.cleber@gmail.com>
2021-05-31 22:12:57 -0300

    Learning rate do otimizador depreciado.

--- src/fitness/medmnist.py
+++ src/fitness/medmnist.py
@@ -129,7 +129,7 @@ class medmnist(base_ff):
 
             if 'Lr' in block:
                 args = re.findall('\d+\.\d+', block)
-                optimizer = optimizers.Adam(lr=float(args[0]))
+                optimizer = optimizers.Adam(learning_rate=float(args[0]))
 
         model.add(layers.Dense(params['DATASET_NUM_CLASSES'], activation='softmax'))
         model.summary()
